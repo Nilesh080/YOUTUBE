b017cc7ed67778cf7564d1931d7e85fa65e5d94a
Nilesh Mishra <122158638+Nilesh080@users.noreply.github.com>
2024-05-01 08:24:47 +0530

    Update WatchPage.js

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -59,7 +59,7 @@ const WatchPage = () => {
       </div>
 
       {data && (
-        <p className={`font-bold text-xl px-2 py-1 ${isMenuOpen ? 'mt-[10px] ': 'mt-[-5275px]'}`}>{data?.title}</p>
+        <p className={`font-bold text-xl px-2 py-1 ${isMenuOpen ? 'mt-[10px] ': 'mt-[-5344px]'}`}>{data?.title}</p>
       )}
       <BelowVideoFrame data={data} stats={stats} />
       <div className="">
